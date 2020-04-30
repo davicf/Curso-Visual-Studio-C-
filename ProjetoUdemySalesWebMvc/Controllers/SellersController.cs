38a3e74098682660cf3872836453ac61a10143b8
Davi Carvalho <davi_carvalho1@live.com>
2020-04-30 16:37:16 -0300

    Simple Create form

--- ProjetoUdemySalesWebMvc/Controllers/SellersController.cs
+++ ProjetoUdemySalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoUdemySalesWebMvc.Models;
 using ProjetoUdemySalesWebMvc.Services;
 
 namespace ProjetoUdemySalesWebMvc.Controllers
@@ -20,6 +21,19 @@ namespace ProjetoUdemySalesWebMvc.Controllers
         {
             var list = _sellerSevice.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerSevice.Insert(seller);
+            return RedirectToAction(nameof(Index)); ;
         }
     }
 }
