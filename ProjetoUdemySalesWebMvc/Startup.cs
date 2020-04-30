b5de01bfe5b3bb96fe47c038799fed4e21b59fdc
Davi Carvalho <davi_carvalho1@live.com>
2020-04-30 12:31:03 -0300

    Sedding Service

--- ProjetoUdemySalesWebMvc/Startup.cs
+++ ProjetoUdemySalesWebMvc/Startup.cs
@@ -40,14 +40,18 @@ namespace ProjetoUdemySalesWebMvc
                     options.UseMySql(Configuration.GetConnectionString("ProjetoUdemySalesWebMvcContext"), builder =>
                     builder.MigrationsAssembly("ProjetoUdemySalesWebMvc")));
 
+            services.AddScoped<SeedingService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            //Verifica se está em desenvolvimento ou produção
             if(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
